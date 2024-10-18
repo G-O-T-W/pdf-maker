120bf7e7f3d4da48ee1b7bf16bc28ab8bcfb6a84
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2024-10-18 15:21:57 +0530

    Add footer in each page

--- main.py
+++ main.py
@@ -2,22 +2,37 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+# Remove auto page breaks and re-adjust margins
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
-    # Add header page
+    # Add the topic title page
     pdf.add_page()
 
+    # Set the header
     pdf.set_font("Times", size=24, style="B")
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], ln=1, align="l")
     pdf.line(10, 22, 200, 22)
 
+    # Set the footer
+    pdf.ln(265)
+    pdf.set_font("Times", size=8, style="I")
+    pdf.set_text_color(160, 160, 160)
+    pdf.cell(w=0, h=4, txt=row['Topic'], align="R")
+
+
     # Add extra pages
     extra_page = row['Pages'] - 1
     for i in range(extra_page):
         pdf.add_page()
+        # Set the footer
+        pdf.ln(277)
+        pdf.set_font("Times", size=8, style="I")
+        pdf.set_text_color(160, 160, 160)
+        pdf.cell(w=0, h=4, txt=row['Topic'], align="R")
 
 # Create pdf
 pdf.output("output.pdf")
