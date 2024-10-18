f4bcffd3bcac283b1fc71e2a34933d34034201a2
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2024-10-18 15:08:59 +0530

    Add multiple pages under each topic

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
+    # Add header page
     pdf.add_page()
 
     pdf.set_font("Times", size=24, style="B")
@@ -13,4 +14,10 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row['Topic'], ln=1, align="l")
     pdf.line(10, 22, 200, 22)
 
+    # Add extra pages
+    extra_page = row['Pages'] - 1
+    for i in range(extra_page):
+        pdf.add_page()
+
+# Create pdf
 pdf.output("output.pdf")
